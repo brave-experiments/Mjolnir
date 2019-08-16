fa002e85442b89fa4e374673335ddd2c2fc325ec
blazejkrzak <blazej@m8b.pl>
2019-08-16 21:10:31 +0200

    Feature: 19 go modules with working tests and go.mod tidy
    Feature: 02 terranova
    
    - added reader of .tf
    
    - added recipes list, adding and default list

--- terra/recipes_test.go
+++ terra/recipes_test.go
@@ -1,6 +1,7 @@
 package terra
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -17,6 +18,32 @@ func TestRecipes_AddRecipe(t *testing.T) {
 	recipes := Recipes{}
 	err := recipes.AddRecipe(keyName, dummyRecipe)
 	assert.Nil(t, err)
+	assert.Equal(t, recipes.Elements[keyName], dummyRecipe)
+}
+
+func TestRecipes_AddRecipeToDefaults(t *testing.T) {
+	keyName := "dummy"
+	dummyRecipe := Recipe{Location: keyName}
+	recipes := Recipes{}
+	recipes.CreateWithDefaults()
+	err := recipes.AddRecipe(keyName, dummyRecipe)
+	assert.Nil(t, err)
+	assert.Equal(t, recipes.Elements[keyName], dummyRecipe)
+}
+
+func TestRecipes_AddRecipeFailure(t *testing.T) {
+	keyName := "dummy"
+	dummyRecipe := Recipe{Location: keyName}
+	recipes := Recipes{}
+	err := recipes.AddRecipe(keyName, dummyRecipe)
+	assert.Nil(t, err)
+	assert.Equal(t, recipes.Elements[keyName], dummyRecipe)
+
+	err = recipes.AddRecipe(keyName, dummyRecipe)
+	// Test that list returned error caused by existing key
+	assert.IsType(t, RecipesError{}, err)
+	assert.Equal(t, err.Error(), fmt.Sprintf("%s  already exists in recipes list", keyName))
+	// Test that list is immutable to error
 	assert.Equal(t, recipes.Elements[keyName], dummyRecipe)
 }
 
