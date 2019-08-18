b33ff4ade0825e55612e85bb76e9a4433e161917
blazejkrzak <blazej@m8b.pl>
2019-08-18 16:18:29 +0200

    Feature: 19 go modules with working tests and go.mod tidy
    Feature: 02 terranova
    
    - added reader of .tf
    
    - added recipes list, adding and default list, moved into one file
    
    - added terranova plus moved src and vendor pattern
    
    - added aws provider
    - added missing vendor dependencies
    - Working with vendoring
    - added default state file
    - added variables pattern to file struct

--- terra/terra.go
+++ terra/terra.go
@@ -9,17 +9,41 @@ import (
 type Client struct {
 	Recipes  Recipes
 	Provider terraform.ResourceProvider
-	platform terranova.Platform
-	state    terranova.State
+	state    *StateFile
+	platform *terranova.Platform
 }
 
-func (client *Client) DefaultClient() {
+func (client *Client) RunPlatform(file File) (err error) {
+	err = file.ReadFile()
+
+	if nil != err {
+		return err
+	}
+
+	if nil == client.platform {
+		return ClientError{"Platform is not initialized"}
+	}
+
+	for key, value := range file.Variables {
+		client.platform.Var(key, value)
+	}
+
+	return nil
+}
+
+func (client *Client) DefaultClient() (err error) {
+	// Maybe recipes should not be registered within client?
 	client.Recipes = Recipes{}
 	client.Recipes.CreateWithDefaults()
-	client.platform = terranova.Platform{
+	client.platform = &terranova.Platform{
 		Providers: make(map[string]terraform.ResourceProvider),
 	}
 	client.platform.AddProvider(DefaultProvider("aws"))
+
+	state, err := DefaultStateFile()
+	client.state = state
+
+	return err
 }
 
 func DefaultProvider(key string) (returnKey string, provider terraform.ResourceProvider) {
