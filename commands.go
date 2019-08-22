574876d681fe40365790ec310e651b5242bb7425
blazejkrzak <blazej@m8b.pl>
2019-08-22 19:26:14 +0200

    Feature: 26 apply command implementation
    - working tests
    - Added print messages

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "github.com/brave-experiments/apollo-devops/terra"
     "github.com/mitchellh/cli"
 )
@@ -35,11 +36,20 @@ func ApplyCmdFactory() (command cli.Command, err error) {
 }
 
 func (applyCmd ApplyCmd) Run(args []string) (exitCode int) {
-    if len(args) < 1 {
+    expectedMinimumArguments := 1
+
+    if len(args) < expectedMinimumArguments {
+        fmt.Printf(
+            "Not enough arguments, expected more than %v \n",
+            expectedMinimumArguments,
+        )
+
         return ExitCodeInvalidNumOfArgs
     }
 
     if nil == &applyCmd.Recipes || len(applyCmd.Recipes.Elements) < 1 {
+        fmt.Println("Invalid recipes configuration")
+
         return ExitCodeInvalidSetup
     }
 
@@ -48,8 +58,19 @@ func (applyCmd ApplyCmd) Run(args []string) (exitCode int) {
     _, contains := recipes[recipeKey]
 
     if false == contains {
+        fmt.Printf(
+            "Recipe %s not found within recipes, available are: \n",
+            recipeKey,
+        )
+        applyCmd.printRecipesKeys()
+
         return ExitCodeInvalidArgument
     }
+
+    fmt.Printf(
+        "Executing %s \n",
+        recipeKey,
+    )
 
     return ExitCodeSuccess
 }
@@ -61,5 +82,15 @@ func (applyCmd ApplyCmd) Help() (helpMessage string) {
 func (applyCmd ApplyCmd) Synopsis() (synopsis string) {
     synopsis = "apply [recipe] [optional]"
     return synopsis
+}
+
+func (applyCmd ApplyCmd) printRecipesKeys() {
+    if nil == &applyCmd.Recipes {
+        return
+    }
+
+    for key := range applyCmd.Recipes.Elements {
+        fmt.Println(key)
+    }
 }
 
