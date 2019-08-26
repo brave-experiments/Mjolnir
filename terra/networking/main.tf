552b671d3cc28b30b8cac6bdf64af2149fb72718
Jerzy Kopaczewski <jerzy.kopaczewski@yupo.pl>
2019-08-26 08:39:41 +0200

    #29 - move to local state config

--- terra/networking/main.tf
+++ terra/networking/main.tf
@@ -1,9 +1,3 @@
-terraform {
-  backend "s3" {
-    # backend configuration is auto discovered by running Terraform inside _terraform_init folder
-  }
-}
-
 provider "aws" {
   region  = "${var.region}"
   version = "~> 1.36"
