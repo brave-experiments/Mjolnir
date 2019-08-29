7c1528648ce47273b5a768e58e3aa6dd26f3dbd2
blazejkrzak <blazej@m8b.pl>
2019-08-29 10:38:14 +0200

    Feature: 48 vars from yaml file
    - validation of yaml file
    - removed modules logic
    - schema reads variables and is guarded

--- terra/schema.go
+++ terra/schema.go
@@ -8,17 +8,23 @@ import (
 
 type VariablesSchema struct {
 	File
-	Type string
+	Type    string
+	Version float64
 }
 
 type variablesModel struct {
-	ApolloSchema string                 `yaml:"apolloSchema"`
+	Version      float64                `yaml:"version"`
 	ResourceType string                 `yaml:"resourceType"`
 	Variables    map[string]interface{} `yaml:"variables"`
 }
 
+const (
+	CurrentVersion = float64(0.1)
+)
+
 var (
-	SupportedTypes = []string{".yml", ".yaml"}
+	SupportedFileTypes     = []string{".yml", ".yaml"}
+	SupportedResourceTypes = []string{"variables"}
 )
 
 //func (variablesSchema *VariablesSchema) ReadFile() (err error) {
@@ -46,7 +52,21 @@ func (variablesSchema *VariablesSchema) guard() (err error) {
 
 	varsModel := variablesModel{}
 
-	err = yaml.Unmarshal([]byte(variablesSchema.Body), varsModel)
+	err = yaml.Unmarshal([]byte(variablesSchema.Body), &varsModel)
+
+	if nil != err {
+		return err
+	}
+
+	err = varsModel.guard()
+
+	if nil != err {
+		return err
+	}
+
+	variablesSchema.Variables = varsModel.Variables
+	variablesSchema.Type = varsModel.ResourceType
+	variablesSchema.Version = varsModel.Version
 
 	return err
 }
@@ -54,16 +74,74 @@ func (variablesSchema *VariablesSchema) guard() (err error) {
 func guardExtension(filePath string) (err error) {
 	fileExtension := path.Ext(filePath)
 
-	if false == contains(SupportedTypes, fileExtension) {
+	if false == contains(SupportedFileTypes, fileExtension) {
 		return ClientError{fmt.Sprintf(
-			"%s is not in supported types. Valid are: %s",
+			"%s is not in supported file types. Valid are: %s",
 			fileExtension,
-			SupportedTypes,
+			SupportedFileTypes,
 		),
 		}
 	}
 
-	return nil
+	return
+}
+
+func (variablesModel *variablesModel) guard() (err error) {
+	err = variablesModel.guardVersion()
+
+	if nil != err {
+		return err
+	}
+
+	err = variablesModel.guardResourceType()
+
+	if nil != err {
+		return err
+	}
+
+	err = variablesModel.guardVariables()
+
+	return err
+}
+
+func (variablesModel *variablesModel) guardVersion() (err error) {
+	version := variablesModel.Version
+
+	if version > CurrentVersion {
+		return ClientError{fmt.Sprintf(
+			"%v version is not supported. Current version: %v",
+			version,
+			CurrentVersion,
+		),
+		}
+	}
+
+	return
+}
+
+func (variablesModel *variablesModel) guardResourceType() (err error) {
+	resourceType := variablesModel.ResourceType
+
+	if false == contains(SupportedResourceTypes, resourceType) {
+		return ClientError{fmt.Sprintf(
+			"%s is not in supported resource types. Valid are: %s",
+			resourceType,
+			SupportedResourceTypes,
+		),
+		}
+	}
+
+	return
+}
+
+func (variablesModel *variablesModel) guardVariables() (err error) {
+	variables := variablesModel.Variables
+
+	if nil == variables {
+		return ClientError{"No variables found"}
+	}
+
+	return
 }
 
 func contains(haystack []string, needle string) bool {
@@ -72,5 +150,6 @@ func contains(haystack []string, needle string) bool {
 			return true
 		}
 	}
+
 	return false
 }
--- terra/schema_test.go
+++ terra/schema_test.go
@@ -8,15 +8,23 @@ import (
 )
 
 const (
-	YamlV1Fixture = `
-apolloSchema: 0.1
+	YamlV1Fixture = `version: 0.1
 resourceType: variables
-  variables: 
-    simpleKey: variable
+variables: 
+  simpleKey: variable
+`
+	YamlFixtureConfigurable = `version: %v
+resourceType: %s
+variables:
+  simpleKey: variable
+`
+
+	YamlFixtureNoVariables = `version: 0.1
+resourceType: variables
 `
 
 	NoSuchFileOrDirectoryMsg = "open %s: no such file or directory"
-	NotValidExtMsg           = "%s is not in supported types. Valid are: [.yml .yaml]"
+	NotValidExtMsg           = "%s is not in supported file types. Valid are: [.yml .yaml]"
 )
 
 func TestVariablesSchema_ReadFailure(t *testing.T) {
@@ -25,8 +33,8 @@ func TestVariablesSchema_ReadFailure(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(
 		t,
-		err.Error(),
 		fmt.Sprintf(NoSuchFileOrDirectoryMsg, variablesSchema.Location),
+		err.Error(),
 	)
 
 	variablesSchema.Location = "non-existing.yml"
@@ -34,8 +42,8 @@ func TestVariablesSchema_ReadFailure(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(
 		t,
-		err.Error(),
 		fmt.Sprintf(NoSuchFileOrDirectoryMsg, variablesSchema.Location),
+		err.Error(),
 	)
 
 	// It fails on invalid file type
@@ -47,8 +55,8 @@ func TestVariablesSchema_ReadFailure(t *testing.T) {
 	assert.IsType(t, ClientError{}, err)
 	assert.Equal(
 		t,
-		err.Error(),
 		fmt.Sprintf(NotValidExtMsg, path.Ext(dummyFilePath)),
+		err.Error(),
 	)
 	RemoveDummyFile(t, dummyFilePath)
 
@@ -60,4 +68,77 @@ func TestVariablesSchema_ReadFailure(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "yaml: line 1: did not find expected ',' or '}'", err.Error())
 	RemoveDummyFile(t, dummyFilePath)
+}
+
+func TestVariablesSchema_ReadFailure_BodyParsing(t *testing.T) {
+	// It fails on invalid version
+	variablesSchema := VariablesSchema{}
+	dummyFilePath := "dummy.yml"
+	PrepareDummyFile(t, dummyFilePath, "{Some string:\n\t\tkk:\nx}")
+	variablesSchema.Location = dummyFilePath
+	err := variablesSchema.Read()
+	assert.Error(t, err)
+	assert.Equal(t, "yaml: line 1: did not find expected ',' or '}'", err.Error())
+	RemoveDummyFile(t, dummyFilePath)
+
+	// It fails on invalid resource version
+	version := float64(2)
+	resource := "dummyResource"
+	configuredYaml := configureYaml(version, resource)
+	PrepareDummyFile(t, dummyFilePath, configuredYaml)
+	err = variablesSchema.Read()
+	assert.Error(t, err)
+	assert.Equal(
+		t,
+		fmt.Sprintf("%v version is not supported. Current version: %v", version, CurrentVersion),
+		err.Error(),
+	)
+
+	// It fails on invalid resource type
+	version = float64(0.1)
+	configuredYaml = configureYaml(version, resource)
+	PrepareDummyFile(t, dummyFilePath, configuredYaml)
+	err = variablesSchema.Read()
+	assert.Error(t, err)
+	assert.Equal(
+		t,
+		fmt.Sprintf("%s is not in supported resource types. Valid are: %s", resource, SupportedResourceTypes),
+		err.Error(),
+	)
+	RemoveDummyFile(t, dummyFilePath)
+
+	// It fails when no variables are present
+	version = float64(0.1)
+	PrepareDummyFile(t, dummyFilePath, YamlFixtureNoVariables)
+	err = variablesSchema.Read()
+	assert.Error(t, err)
+	assert.Equal(
+		t,
+		"No variables found",
+		err.Error(),
+	)
+	RemoveDummyFile(t, dummyFilePath)
+}
+
+func TestVariablesSchema_Read(t *testing.T) {
+	variablesSchema := VariablesSchema{}
+	dummyFilePath := "dummy.yml"
+	PrepareDummyFile(t, dummyFilePath, YamlV1Fixture)
+	variablesSchema.Location = dummyFilePath
+	err := variablesSchema.Read()
+	assert.Nil(t, err)
+	assert.Equal(t, variablesSchema.Version, float64(0.1))
+	assert.Equal(t, variablesSchema.Type, "variables")
+	assert.NotNil(t, variablesSchema.Variables)
+	variables := variablesSchema.Variables
+	assert.Equal(t, "variable", variables["simpleKey"])
+	RemoveDummyFile(t, dummyFilePath)
+}
+
+func configureYaml(version float64, resourceType string) (fileBody string) {
+	return fmt.Sprintf(
+		YamlFixtureConfigurable,
+		version,
+		resourceType,
+	)
 }
