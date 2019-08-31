3c9d476a913adc7fbdde4d8368bfabd75c8e24b5
blazejkrzak <blazej@m8b.pl>
2019-08-31 13:29:00 +0200

    Feature: #31 static assets to binary
    - builder can attach recipes to binary

--- builder/builder.go
+++ builder/builder.go
@@ -2,17 +2,18 @@ package builder
 
 import (
 	"bytes"
-	"fmt"
-	"html/template"
+	"golang.org/x/net/html"
+	"strings"
+	"text/template"
 )
 
 const (
 	StaticFilePath     = "static.go"
 	StaticFileTemplate = `
-package %s
+package {{with .PackageName}}{{index .}}{{else}}builder{{end}}
 
 var (
-{{range $key, $value := .StaticVariables}}{{$key}} = {{$value}}{{end}}
+{{range $key, $value := .StaticVariables}}{{$key}} = &#96;{{$value}}&#96;{{end}}
 )
 `
 )
@@ -21,12 +22,13 @@ var (
 	DefaultFilePath = StaticFilePath
 )
 
-func Build(packageName string, staticVariables map[string]interface{}) (result string, err error) {
-	staticFileTemplate := fmt.Sprintf(StaticFileTemplate, packageName)
+func Build(staticVariables map[string]interface{}) (result string, err error) {
 	bytesBuffer := bytes.Buffer{}
-	tmpl := template.Must(template.New(DefaultFilePath).Parse(staticFileTemplate))
+
+	tmpl := template.Must(template.New(DefaultFilePath).Parse(StaticFileTemplate))
 	err = tmpl.Execute(&bytesBuffer, staticVariables)
 	result = bytesBuffer.String()
+	result = strings.Replace(result, "`", "", -1)
 
-	return result, err
+	return html.UnescapeString(result), err
 }
--- builder/builder_test.go
+++ builder/builder_test.go
@@ -1,8 +1,10 @@
 package builder
 
 import (
-	"github.com/brave-experiments/apollo-devops/src/github.com/stretchr/testify/assert"
 	"github.com/brave-experiments/apollo-devops/terra"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/html"
+	"os"
 	"testing"
 )
 
@@ -13,12 +15,22 @@ func TestBuild(t *testing.T) {
 		},
 	}
 
-	result, err := Build("builder", staticVariablesMap)
+	result, err := Build(staticVariablesMap)
 	assert.Nil(t, err)
-	assert.Equal(t, "\npackage builder\n\nvar (\nkey1 = var1\n)\n", result)
+	assert.Equal(
+		t,
+		html.UnescapeString("\npackage builder\n\nvar (\nkey1 = &#96;var1&#96;\n)\n"),
+		result,
+	)
 }
 
 func TestBuild_BuildRecipe(t *testing.T) {
+	currentDir, err := os.Getwd()
+	assert.Nil(t, err)
+
+	err = os.Chdir("../")
+	assert.Nil(t, err)
+
 	recipes := terra.DefaultRecipes
 	staticVariables := make(map[string]string)
 
@@ -29,10 +41,18 @@ func TestBuild_BuildRecipe(t *testing.T) {
 	}
 
 	staticVariablesMap := map[string]interface{}{
+		"PackageName":     "terra",
 		"StaticVariables": staticVariables,
 	}
 
-	result, err := Build("terra", staticVariablesMap)
+	result, err := Build(staticVariablesMap)
 	assert.Nil(t, err)
-	assert.Nil(t, result)
+	file, err := os.Create("dupa.go")
+	assert.Nil(t, err)
+	_, err = file.Write([]byte(result))
+	assert.Nil(t, err)
+	assert.Equal(t, "", result)
+
+	err = os.Chdir(currentDir)
+	assert.Nil(t, err)
 }
