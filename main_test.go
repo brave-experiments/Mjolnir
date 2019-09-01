88f374ce2bd8bc409cd326aec87d9cc5263f28b6
blazejkrzak <blazej@m8b.pl>
2019-09-01 08:21:48 +0200

    Feature: #76 recipe-body-parsing-process

--- main_test.go
+++ main_test.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"os"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 	cli := New()
-	assert.Equal(t, cli.Name, os.Getenv("CLI_NAME"))
+	assert.Equal(t, cli.Name, CliName)
 	assert.Equal(t, cli.Commands, RegisteredCommands)
 }
