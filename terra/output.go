5499250c6bf2a13c0f3df38cea6e71b2f3aa777b
blazejkrzak <blazej@m8b.pl>
2019-09-05 13:43:16 +0200

    Feature: 92 output verbosity
    - change within tests
    - moved to interface

--- terra/output.go
+++ terra/output.go
@@ -1,1 +1,25 @@
 package terra
+
+import "github.com/tidwall/gjson"
+
+const (
+	ModulesLocator = "modules.outputs"
+)
+
+type OutputRecord struct {
+	Name      string
+	Sensitive bool        `json:"sensitive"`
+	Type      string      `json:"type"`
+	Value     interface{} `json:"value"`
+}
+
+func (outputRecord *OutputRecord) ParseOutputsFromJson(jsonBody string) {
+	outputLocator := ModulesLocator + "." + outputRecord.Name
+	jsonOutputs := gjson.Get(jsonBody, outputLocator)
+
+	if false == jsonOutputs.Exists() {
+		return
+	}
+
+	//outputRecord.Sensitive = jsonOutputs.Exists()
+}
