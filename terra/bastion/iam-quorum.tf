4e6c51f2e0207a6db1840c5bb7556bd9de94f181
Genysys <samuel.b.dare@gmail.com>
2019-12-17 07:23:37 +0000

    Phantom file fix

---
+++ terra/bastion/iam-quorum.tf
@@ -0,0 +1,114 @@
+resource "aws_iam_role" "bastion" {
+  name = "${local.default_bastion_resource_name}"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "bastion" {
+  statement {
+    sid = "AllowS3"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.quorum_bucket}",
+      "arn:aws:s3:::${local.quorum_bucket}/*",
+    ]
+  }
+
+  statement {
+    sid = "AllowS3Bastion"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bastion_bucket}",
+      "arn:aws:s3:::${local.bastion_bucket}/*",
+    ]
+  }
+
+  statement {
+    sid = "AllowKMSAccess"
+
+    actions = [
+      "kms:*",
+    ]
+
+    resources = [
+      "${aws_kms_key.bucket.arn}",
+    ]
+  }
+
+  statement {
+    sid = "AllowECS"
+
+    actions = [
+      "ecs:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = "AllowEC2"
+
+    actions = [
+      "ec2:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = "AllowSQS"
+
+    actions = [
+      "sqs:*",
+    ]
+
+    resources = [
+      "${aws_sqs_queue.faketime_queue.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.default_bastion_resource_name}"
+  role = "${aws_iam_role.bastion.name}"
+}
+
+resource "aws_iam_policy" "bastion" {
+  name        = "quorum-bastion-policy-${var.network_name}"
+  path        = "/"
+  description = "This policy allows task to access S3 bucket and ECS"
+  policy      = "${data.aws_iam_policy_document.bastion.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = "${aws_iam_role.bastion.id}"
+  policy_arn = "${aws_iam_policy.bastion.arn}"
+}
