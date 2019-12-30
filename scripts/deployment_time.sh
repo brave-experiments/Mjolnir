98fae0373770db595fd019e21a102e5bf39df428
Genysys <samuel.b.dare@gmail.com>
2019-12-30 10:23:53 +0000

    Fixed deployememnt timing script

--- scripts/deployment_time.sh
+++ scripts/deployment_time.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 
 # @dev : This is the script measuring deployment and destroy times.
-
-
 set -x
 
 for ((i=4;i<=50;i++)); 
 do 
    # your-unix-command-here
+    mkdir -p results/deploy
+    mkdir -p results/destroy
     sed -i.bak "s/ number_of_nodes:.*/ number_of_nodes: \"${i}\"/" examples/values-local.yml 
-    { time  make quorum ; } 2> results/deploy${i}.txt
-    { time  make destroy ; } 2> results/deploy${i}.txt
+    { time  make quorum ; } 2> results/deploy/deploy${i}.txt
+    { time  make destroy ; } 2> results/destroy/deploy${i}.txt
     echo "${i} done!!!"
 
 done
